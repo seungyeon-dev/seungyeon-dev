d01225c377e5fd5fc8f0794e7042c6f1acb62bc6
seungyeon-dev <kiwibirdwiki@gmail.com>
2023-10-01 14:56:22 +0900

    chore: 미사용 파일 삭제 및 컴포넌트 네이밍 변경

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # TODO
 
 1. About 콘텐츠 작성
+
+- 레퍼런스
+  - https://www.howdy-mj.me/about
+  - https://miryang.dev/about
+
 2. 블로그 포스팅 하나 이전하기 (노션에 있는것)
-3. 코드 정리
 
-- 컴포넌트 네이밍
-
-4. 배포
+3. 배포
 
 - https://nextjs.org/docs/pages/building-your-application/deploying
 - https://github.com/vercel/next.js/tree/canary/examples/github-pages
+
+6. 댓글기능 추가
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { allPosts } from "@/.contentlayer/generated"
-import Link from "next/link"
+import { allPosts } from '@/.contentlayer/generated';
+import Link from 'next/link';
 
-export default function Home() {
+export default function Blog() {
   return (
-    <div className="prose dark:prose-invert">
+    <div className='prose dark:prose-invert'>
       {allPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
@@ -13,5 +13,5 @@ export default function Home() {
         </article>
       ))}
     </div>
-  )
+  );
 }
